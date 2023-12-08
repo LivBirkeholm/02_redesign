fe702e9e68450c572a8422129e3cbd1d59ff3cdb
Liv <libi0001@stud.kea.dk>
2023-12-08 11:47:04 +0100

    java på mobilsite

--- img.js
+++ img.js
@@ -1,11 +1,19 @@
 function openModal(imgSrc) {
-  var modal = document.getElementById("imageModal");
-  var img = document.getElementById("enlargedImg");
-  img.src = imgSrc;
-  modal.style.display = "block";
+  if (window.matchMedia("(min-width: 800px)").matches) {
+    var modal = document.getElementById("imageModal");
+    var img = document.getElementById("enlargedImg");
+    img.src = imgSrc;
+    modal.style.display = "block";
+  }
 }
 
 function closeModal() {
   var modal = document.getElementById("imageModal");
   modal.style.display = "none";
 }
+
+window.addEventListener("resize", function () {
+  if (window.innerWidth < 800) {
+    closeModal();
+  }
+});
